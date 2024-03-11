0f21e5fc8d7e5fa961cd6b76848e5d30c207e036
Frankkojee <110838835+Frankkojee@users.noreply.github.com>
2024-03-11 23:29:55 +0300

    ..

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Problem Of the day when I joined leetcode for the first time");
+    }
+}
